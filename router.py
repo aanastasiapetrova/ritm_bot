40fe8b911a6a49180bd0b8b5add108e0229e13dc
Anastasia Petrova <an.a.petrova@rt.ru>
2024-11-10 21:37:09 +0400

    base keyboard init

--- router.py
+++ router.py
@@ -3,12 +3,14 @@ from aiogram import Router, html
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from app import kb
+
 basic_router = Router()
 
 @basic_router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f'Здравствуйте, {html.bold(message.from_user.full_name)}! '
-                         f'Здесь вы можете получить подписку на закрытый клуб!')
+                         f'Здесь вы можете получить подписку на закрытый клуб!', reply_markup=kb.subscribe())
 
 
 @basic_router.message()
